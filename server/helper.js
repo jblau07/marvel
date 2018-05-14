841a948798d932a69ee736b1b9977b279b1021ba
jblau07 <jlau7248@gmail.com>
2018-05-14 12:58:11 -1000

    updated to take in name parameter

--- server/helper.js
+++ server/helper.js
@@ -5,11 +5,11 @@ module.exports = {
 const CryptoJS = require('crypto-js');
 const keys = require('../src/config');
 
-function getResponse() {
+function getResponse(name) {
   let ts = new Date().getTime();
   let hash = CryptoJS.MD5(ts + keys.api.private + keys.api.public);
 
-  let url = 'https://gateway.marvel.com:443/v1/public/characters?name=spider-man&';
+  let url = `https://gateway.marvel.com:443/v1/public/characters?name=${name}&`;
 
   return `${url}ts=${ts}&apikey=${keys.api.public}&hash=${hash}
     `
