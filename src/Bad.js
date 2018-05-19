bfc8936e04678605dedeb983a5dd6e684d45ae61
jblau07 <jlau7248@gmail.com>
2018-05-18 14:18:18 -1000

    added bad path component

---
+++ src/Bad.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Bad = props => {
+  return (
+    <div className="no-match">
+      <h2>No match found for requested path</h2>
+    </div>
+  )
+}
+
+export default Bad
