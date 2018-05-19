c8527c3799ac9627a81d1c27c13989191455a128
jblau07 <jlau7248@gmail.com>
2018-05-18 14:17:44 -1000

    updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose} from 'redux';
+import { Provider } from 'react-redux';
+
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const boundCompose = compose.bind(null, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? boundCompose(window.__REDUX_DEVTOOLS_EXTENSION__()) : boundCompose()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+   </Provider>,
+   document.getElementById('root'));
+  registerServiceWorker();
+
+
