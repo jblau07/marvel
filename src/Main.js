618af78cf57535dbe6fc6cf429cffea393f1af05
jblau07 <jlau7248@gmail.com>
2018-05-24 12:37:18 -1000

    updated to add search route

--- src/Main.js
+++ src/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Search from './Search';
+import Hero from './Hero';
 import Bad from './Bad';
 
 const Main = (props) => {
@@ -9,6 +10,7 @@ const Main = (props) => {
     <div className="page-content">
       <Switch>
         <Route exact path="/" component={ Search } />
+        <Route exact path="/search" component={ Hero } />
         <Route component={ Bad } />
       </Switch>
     </div>
