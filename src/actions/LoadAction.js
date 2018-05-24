7860c0f1480dedbcca8b10acb8009ec4f51e6f9d
jblau07 <jlau7248@gmail.com>
2018-05-24 12:38:01 -1000

    added if statement if character not found, added redirect callback

--- src/actions/LoadAction.js
+++ src/actions/LoadAction.js
@@ -1,20 +1,33 @@
 import axios from 'axios';
 export const LOAD_HERO = 'LOAD_HERO';
 
-export const loadHero = (hero) => {
+export const loadHero = (hero, redirectCallback) => {
+  let thisHero = encodeURIComponent(hero.trim()) 
+
   return dispatch => {
     return axios.post('/api/hero', {
-      name: hero
+      name: thisHero
     })
     .then(data => {
+      let heroData;
+      if (data.data.data.results.length === 0) {
+        heroData = null;
+      } else {
+        heroData = {
+          name: data.data.data.results[0].name,
+          description: data.data.data.results[0].description,
+          image: `${data.data.data.results[0].thumbnail.path}/portrait_medium.jpg`
+        }
+      }
+
       dispatch({
-        type: 'LOAD_HERO',
-        hero: data
+        type: LOAD_HERO,
+        hero: heroData
       })
+      redirectCallback()
     })
     .catch(err => {
       console.log({err: err.message})
     })
   }
-
 }
